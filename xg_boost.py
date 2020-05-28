a712a923b9cc3aad29ca8090075a4914f1612a93
Oliver226698 <53034840+Oliver226698@users.noreply.github.com>
2020-05-28 21:53:04 +0800

    Add files via upload

---
+++ xg_boost.py
@@ -0,0 +1,48 @@
+# XGBoost
+
+# Installing XGBoost
+# Enter the following command in a terminal (or anaconda prompt for Windows users): conda install -c conda-forge xgboost
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+# 這邊有替換新的程式碼，不然原本的跑不出來，可能是套件有更新
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+label_encoder_X_1 = LabelEncoder()
+X[:, 1] = label_encoder_X_1.fit_transform(X[:, 1])
+label_encoder_X_2 = LabelEncoder()
+X[:, 2] = label_encoder_X_2.fit_transform(X[:, 2])
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [1])], remainder = 'passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+X = X[:, 1:]  
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting XGBoost to the Training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
